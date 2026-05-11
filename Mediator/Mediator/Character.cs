d252d7b2f760b917671158e61c809b09de742889
Tamir Yamaleyev <tamirka8@gmail.com>
2026-05-11 19:01:51 +0300

    Create base character

---
+++ Mediator/Mediator/Character.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator
+{
+    internal class Character
+    {
+        private string name;
+        private float maxHP;
+        private float currentHP;
+        private float damage;
+        private bool isDead = false;
+
+        public string Name => name;
+        public float CurrentHP => currentHP;
+        public float Damage => damage;
+        public bool IsDead => isDead;
+
+        public Character(string name, float maxHP, float damage)
+        {
+            this.name = name;
+            this.maxHP = maxHP;
+            currentHP = maxHP;
+            this.damage = damage;
+        }
+
+        public void TakeDamage(Character actor)
+        {
+            currentHP -= actor.Damage;
+
+            if (currentHP <= 0)
+            {
+                currentHP = 0;
+                isDead = true;
+            }
+        }
+
+        public void Heal(float amount)
+        {
+            currentHP += amount;
+
+            if (currentHP > maxHP) 
+                currentHP = maxHP;
+        }
+
+        public void DealDamage(Character victim)
+        {
+            victim.TakeDamage(damage);
+        }
+    }
+}
