88938a1847587dd76b1360b06fcaa3be9f943aa3
CutebatDev <alexaverten@gmail.com>
2026-05-11 18:53:00 +0300

    ui push

---
+++ Mediator/Mediator/UI.cs
@@ -0,0 +1,33 @@
+﻿public class UI
+{
+    private int playerHealth
+    {
+        set
+        {
+            playerHealth = value;
+            updateUI();
+        }
+        get
+        {
+            return playerHealth;
+        }
+    };
+    private int enemyHealth
+    {
+        set
+        {
+            enemyHealth = value;
+            updateUI();
+        }
+        get
+        {
+            return enemyHealth;
+        }
+    };
+
+    private updateUI()
+    {
+        Console.WriteLine($"Player health : {playerHealth}");
+        Console.WriteLine($"Enemy health : {enemyHealth}");
+    }
+}
