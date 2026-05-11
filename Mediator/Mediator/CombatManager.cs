1f476c2997154900bf512a088036d62d189d21ed
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-05-11 18:50:54 +0300

    .

---
+++ Mediator/Mediator/CombatManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Mediator;
+
+/// <summary>
+/// Manages characters dealing damage to each other and updates the UI
+/// </summary>
+public class CombatManager
+{
+    //Character player, enemy;
+    //UI ui;
+
+    public CombatManager()
+    {
+        // Set characters and UI
+    }
+    
+    public void PlayerDealDamage()
+    {
+        Console.WriteLine("Player Deals Damage");
+        Console.WriteLine("Updating UI");
+    }
+
+    public void EnemyDealDamage()
+    {
+        Console.WriteLine("Enemy Deals Damage");
+        Console.WriteLine("Updating UI");
+    }
+}
